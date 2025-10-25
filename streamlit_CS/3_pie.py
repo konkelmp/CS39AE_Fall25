13a97890e0eb2534d74623b80a294dc6221feeb6
Preston Konkel <80935110+konkelmp@users.noreply.github.com>
2025-10-24 23:39:20 -0600

    Update pie.py to display DataFrame instead of pie chart
    Removed pie chart visualization and added DataFrame display.

--- streamlit_CS/3_pie.py
+++ streamlit_CS/3_pie.py
@@ -1,16 +1,7 @@
+import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
 
-df = pd.read_csv('streamlit_CS/data/pie_demo.csv')
+st.title("New Page Test")
 
-# Extract data
-labels = df['Category']
-sizes = df['Value']
-
-# Create pie chart
-plt.figure(figsize=(6,6))
-plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=140)
-plt.title('Fruit Distribution')
-plt.axis('equal')  # Equal aspect ratio ensures the pie is circular
-plt.tight_layout()
-plt.show()
+df = pd.read_csv("data/pie_demo.csv")
+st.write(df.head())
